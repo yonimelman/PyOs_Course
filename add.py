3fcff512579a961bf0a2b814779b8094888953c7
yoni <yoni.melman@gmail.com>
2015-05-05 22:21:38 +0300

    change error raised to TypeError

--- add.py
+++ add.py
@@ -1,6 +1,18 @@
 def addition(x,y):
+    '''
+    addition(x,y) takes two numbers, adds them and returns their sum.
+    input: 
+        x - number (type int/float)
+        y - number (type int/float)
+    output:
+        the sum of x+y; number (type int/float)
+    '''
+
     if type(x)!=int and type(x)!=float:
-        raise ValueError
+        raise TypeError
     if type(y)!=int and type(y)!=float:
-        raise ValueError
+        raise TypeError
+    
     return x+y
+
+
