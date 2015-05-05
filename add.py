6bfa069ff4bee584580ff1d39b2d7c7b00c939d5
yoni <yoni.melman@gmail.com>
2015-05-05 21:26:27 +0300

    indentation change in add.py

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
 def addition(x,y):
     if type(x)!=int and type(x)!=float:
-	raise ValueError
+        raise ValueError
     if type(y)!=int and type(y)!=float:
         raise ValueError
     return x+y
