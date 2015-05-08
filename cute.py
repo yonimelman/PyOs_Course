54ce74fd493b6efc39c9eb82430385b145d4f227
yoni <yoni.melman@gmail.com>
2015-05-08 11:00:24 +0300

    add cute.py

---
+++ cute.py
@@ -0,0 +1,12 @@
+def cuteprint(word):
+    '''cuteprint() takes a phrase and adds a cute y in the end
+    examples: dog->dogy, smile->smiley
+    
+    input: a word (type?)
+    output: prints the word + y
+    
+    '''
+    
+    print str(word) + 'y'
+
+
