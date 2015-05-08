01aa5012adb8da2af162ab017467f7c93a4b0356
yoni <yoni.melman@gmail.com>
2015-05-08 10:20:47 +0300

    add double spiral example

---
+++ double_spiral.py
@@ -0,0 +1,18 @@
+﻿import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+size= 100
+for i in range(1,3):
+    for j in range(1,size+1):
+        
+        if i%2 != 0:
+            t.right(30)
+            t.forward(j/2)
+        else:
+            t.left(30)
+            t.forward((size-j)/2)
+
+
+s.exitonclick()
