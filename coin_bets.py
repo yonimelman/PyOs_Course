49f99826f627170df53826992856d4e3c5feb6c0
yoni <yoni.melman@gmail.com>
2015-05-10 21:54:25 +0300

    add coins bet

---
+++ coin_bets.py
@@ -0,0 +1,13 @@
+﻿import random
+
+coin_sides = ('HEADS','TAILS')
+
+bet = raw_input('What is your bet? HEADS or TAILS? ')
+
+#coin flip
+side = random.choice(coin_sides)
+
+if side == bet:
+	print "Well Done!, get 100$"
+else:
+	print "Too bad.., pay 100$"
